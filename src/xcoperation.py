7b97e6965567b3a7f37857a5e7bd72fcbc617467
Ray A <arp.rayappan@gmail.com>
2023-01-29 07:01:54 +0530

    format correction.

--- src/xcoperation.py
+++ src/xcoperation.py
@@ -14,6 +14,7 @@ class ICSXCOperation:
         self.subelem = ''
         self.create_xml_model()
 
+
     def create_xml_model(self) -> None:
         """XML Model creator with XPATH"""
         if isinstance(self.xpath, str):
@@ -32,6 +33,7 @@ class ICSXCOperation:
             self.subelem = ET.SubElement(parent, child)
             parent = self.subelem
 
+
     def _delete(self, xc_element: str, child: str, value: str) -> None:
         """Setting the XC operation delete"""
 
@@ -43,6 +45,7 @@ class ICSXCOperation:
         subelem = ET.SubElement(xc_delete, child)
         subelem.text = value
 
+
     def write_xml(self, file) -> None:
         """Writes the generated Idle XML delete config to a file"""
 
@@ -53,13 +56,14 @@ class ICSXCOperation:
                 fhandle,
                 encoding='unicode',
                 short_empty_elements=False,
-                xml_declaration=False
-                )
+                xml_declaration=False)
+
 
     def dump(self) -> None:
         """Indents and dumps XML tree to stdout"""
         ET.indent(self.root)
         ET.dump(self.root)
+
 
     def signin_url_delete(self, value: Union[str,set]) -> None:
         """Idle SignIn URL delete XML generation"""
@@ -70,6 +74,7 @@ class ICSXCOperation:
             for val in value:
                 self._delete(xc_element="access-url",child="url-pattern",value=val)
 
+
     def realms_delete(self, value: Union[str,set]) -> None:
         """Realms delete XML generation"""
 
@@ -79,6 +84,7 @@ class ICSXCOperation:
             for val in value:
                 self._delete(xc_element="realm",child="name",value=val)
 
+
     def auth_servers_delete(self, value: Union[str,set]) -> None:
         """Auth Servers delete XML generation"""
 
@@ -87,6 +93,7 @@ class ICSXCOperation:
         else:
             for val in value:
                 self._delete(xc_element="auth-server",child="name",value=val)
+
 
     def admin_role_delete(self, value: Union[str,set]) -> None:
         """Auth Servers delete XML generation"""
