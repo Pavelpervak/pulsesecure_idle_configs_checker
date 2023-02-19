1e40bcefba814bf039f6e6a170693a9bd62007ae
Ray A <arp.rayappan@gmail.com>
2023-02-19 07:17:47 +0530

    minor edits

--- src/xpath_rsp.py
+++ src/xpath_rsp.py
@@ -3,20 +3,20 @@
 # Resource profiles.
 
 WEB_PROF_ROOT = './/web-profiles'
-WEB_PROF = './/web-profile'
+WEB_PROF = './/web-profiles/web-profile'
 
 FILE_PROF_ROOT = './/file-win-profiles'
-FILE_PROF = './/file-win-profile'
+FILE_PROF = './/file-win-profiles/file-win-profile'
 
 SAM_PROF_ROOT = './/sam-profiles'
 SAM_PROF_CAPP = './/client-application-profiles/client-application-profile'
 SAM_PROF_DEST = './/wsam-destination-profiles/wsam-destination-profile'
 
 TERMSERV_PROF_ROOT = './/terminal-services-profiles'
-TERMSERV_PROF = './/terminal-services-profile'
+TERMSERV_PROF = './/terminal-services-profiles/terminal-services-profile'
 
 VDI_PROF_ROOT = './/virtual-desktops-profiles'
-VDI_PROF = './/virtual-desktops-profile'
+VDI_PROF = './/virtual-desktops-profiles/virtual-desktops-profile'
 
 HTML5_PROF_ROOT = './/html5-access-profiles'
-HTML5_PROF = './/html5-access-profile'
+HTML5_PROF = './/html5-access-profiles/html5-access-profile'
