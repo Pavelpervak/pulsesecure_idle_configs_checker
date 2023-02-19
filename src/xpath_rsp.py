86bb3d272865d9f16beda550efd5ca5d1c64d219
Ray A <arp.rayappan@gmail.com>
2023-02-19 07:14:40 +0530

    rsprofile xpaths

---
+++ src/xpath_rsp.py
@@ -0,0 +1,22 @@
+"""Resource profiles XPATH"""
+
+# Resource profiles.
+
+WEB_PROF_ROOT = './/web-profiles'
+WEB_PROF = './/web-profile'
+
+FILE_PROF_ROOT = './/file-win-profiles'
+FILE_PROF = './/file-win-profile'
+
+SAM_PROF_ROOT = './/sam-profiles'
+SAM_PROF_CAPP = './/client-application-profiles/client-application-profile'
+SAM_PROF_DEST = './/wsam-destination-profiles/wsam-destination-profile'
+
+TERMSERV_PROF_ROOT = './/terminal-services-profiles'
+TERMSERV_PROF = './/terminal-services-profile'
+
+VDI_PROF_ROOT = './/virtual-desktops-profiles'
+VDI_PROF = './/virtual-desktops-profile'
+
+HTML5_PROF_ROOT = './/html5-access-profiles'
+HTML5_PROF = './/html5-access-profile'
